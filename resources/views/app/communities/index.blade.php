4993ab0a8d132ed2862a82f7ab611e44c0ddd2a8
Supun Nethsara <supunnethsara410@gmail.com>
2024-09-19 05:08:24 -0700

    Update index.blade.php

--- resources/views/app/communities/index.blade.php
+++ resources/views/app/communities/index.blade.php
@@ -26,10 +26,10 @@
                         </div>
                         <div class="text-right md:w-1/2">
                             @can('create', App\Models\Community::class)
-                                <a href="{{ route('communities.create') }}" class="button button-primary">
-                                    <i class="mr-1 icon ion-md-add"></i>
-                                    @lang('crud.common.create')
-                                </a>
+                            <a href="{{ route('communities.create') }}" class="button button-primary">
+                                <i class="mr-1 icon ion-md-add"></i>
+                                @lang('crud.common.create')
+                            </a>
                             @endcan
                         </div>
                     </div>
@@ -64,10 +64,10 @@
                                 {{-- <th class="px-4 py-3 text-left">
                                     @lang('crud.communities.inputs.description')
                                 </th> --}}
-                                {{-- <th class="px-4 py-3 text-left">
+                                <th class="px-4 py-3 text-left">
                                     Amount
                                 </th>
-                                <th class="px-4 py-3 text-left">
+                                {{--<th class="px-4 py-3 text-left">
                                     @lang('crud.communities.inputs.slip')
                                 </th> --}}
                                 <th></th>
@@ -75,82 +75,78 @@
                         </thead>
                         <tbody class="text-gray-600">
                             @forelse($communities as $community)
-                                <tr class="hover:bg-gray-50">
-                                    <td class="px-4 py-3 text-left">
-                                        {{ $community->first_name ?? '-' }}
-                                    </td>
-                                    <td class="px-4 py-3 text-left">
-                                        {{ $community->last_name ?? '-' }}
-                                    </td>
-                                    <td class="px-4 py-3 text-left">
-                                        <a href="mailto:{{ $community->email }}"
-                                            style="color: blue">{{ $community->email ?? '-' }}</a>
-                                    </td>
-                                    <td class="px-4 py-3 text-left">
-                                        {{ $community->country ?? '-' }}
-                                    </td>
-                                    <td class="px-4 py-3 text-left">
-                                        <a href="https://wa.me/{{ $community->phone_number }}" target="_blank"
-                                            style="color: rgb(78, 221, 42)">{{ $community->phone_number ?? '-' }}</a>
-                                    </td>
-                                    <td class="px-4 py-3 text-left">
-                                        {{ $community->date->format('Y-m-d') ?? '-' }}
-                                    </td>
-                                    <td class="px-4 py-3 text-left">
-                                        {{ $community->type ?? '-' }}
-                                    </td>
-                                    {{-- <td class="px-4 py-3 text-left">
+                            <tr class="hover:bg-gray-50">
+                                <td class="px-4 py-3 text-left">
+                                    {{ $community->first_name ?? '-' }}
+                                </td>
+                                <td class="px-4 py-3 text-left">
+                                    {{ $community->last_name ?? '-' }}
+                                </td>
+                                <td class="px-4 py-3 text-left">
+                                    <a href="mailto:{{ $community->email }}"
+                                        style="color: blue">{{ $community->email ?? '-' }}</a>
+                                </td>
+                                <td class="px-4 py-3 text-left">
+                                    {{ $community->country ?? '-' }}
+                                </td>
+                                <td class="px-4 py-3 text-left">
+                                    <a href="https://wa.me/{{ $community->phone_number }}" target="_blank"
+                                        style="color: rgb(78, 221, 42)">{{ $community->phone_number ?? '-' }}</a>
+                                </td>
+                                <td class="px-4 py-3 text-left">
+                                    {{ $community->date->format('Y-m-d') ?? '-' }}
+                                </td>
+                                <td class="px-4 py-3 text-left">
+                                    {{ $community->type ?? '-' }}
+                                </td>
+                                {{-- <td class="px-4 py-3 text-left">
                                     {{ $community->description ?? '-' }}
                                 </td> --}}
-                                    {{-- <td class="px-4 py-3 text-left">
+                                <td class="px-4 py-3 text-left">
                                     {{ number_format($community->amount) ?? '-' }}
                                 </td>
                                 <td class="px-4 py-3 text-left">
                                     @if ($community->slip)
                                     <a
                                         href="{{ \Storage::url($community->slip) }}"
-                                        target="blank"
-                                        ><i
-                                            class="mr-1 icon ion-md-download"
-                                        ></i
-                                        >&nbsp;Download</a
-                                    >
+                                        target="blank"><i
+                                            class="mr-1 icon ion-md-download"></i>&nbsp;Download</a>
                                     @else - @endif
                                 </td> --}}
-                                    <td class="px-4 py-3 text-center" style="width: 134px;">
-                                        <div role="group" aria-label="Row Actions"
-                                            class="relative inline-flex align-middle ">
-                                            @can('update', $community)
-                                                <a href="{{ route('communities.edit', $community) }}" class="mr-1">
-                                                    <button type="button" class="button">
-                                                        <i class="icon ion-md-create"></i>
-                                                    </button>
-                                                </a>
-                                                @endcan @can('view', $community)
-                                                <a href="{{ route('communities.show', $community) }}" class="mr-1">
-                                                    <button type="button" class="button">
-                                                        <i class="icon ion-md-eye"></i>
-                                                    </button>
-                                                </a>
-                                                @endcan @can('delete', $community)
-                                                <form action="{{ route('communities.destroy', $community) }}"
-                                                    method="POST"
-                                                    onsubmit="return confirm('{{ __('crud.common.are_you_sure') }}')">
-                                                    @csrf @method('DELETE')
-                                                    <button type="submit" class="button">
-                                                        <i class="text-red-600  icon ion-md-trash"></i>
-                                                    </button>
-                                                </form>
-                                            @endcan
-                                        </div>
-                                    </td>
-                                </tr>
+                                <td class="px-4 py-3 text-center" style="width: 134px;">
+                                    <div role="group" aria-label="Row Actions"
+                                        class="relative inline-flex align-middle ">
+                                        @can('update', $community)
+                                        <a href="{{ route('communities.edit', $community) }}" class="mr-1">
+                                            <button type="button" class="button">
+                                                <i class="icon ion-md-create"></i>
+                                            </button>
+                                        </a>
+                                        @endcan @can('view', $community)
+                                        <a href="{{ route('communities.show', $community) }}" class="mr-1">
+                                            <button type="button" class="button">
+                                                <i class="icon ion-md-eye"></i>
+                                            </button>
+                                        </a>
+                                        @endcan @can('delete', $community)
+                                        <form action="{{ route('communities.destroy', $community) }}"
+                                            method="POST"
+                                            onsubmit="return confirm('{{ __('crud.common.are_you_sure') }}')">
+                                            @csrf @method('DELETE')
+                                            <button type="submit" class="button">
+                                                <i class="text-red-600  icon ion-md-trash"></i>
+                                            </button>
+                                        </form>
+                                        @endcan
+                                    </div>
+                                </td>
+                            </tr>
                             @empty
-                                <tr>
-                                    <td colspan="9">
-                                        @lang('crud.common.no_items_found')
-                                    </td>
-                                </tr>
+                            <tr>
+                                <td colspan="9">
+                                    @lang('crud.common.no_items_found')
+                                </td>
+                            </tr>
                             @endforelse
                         </tbody>
                         <tfoot>
